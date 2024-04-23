60ea0afcfc3a7cdc683f971d6a7588518ff780c5
kamalperisetti <perisettikamal@gmail.com>
2024-04-23 18:06:53 +0530

    changed direct url access

--- src/components/Submitpage/index.js
+++ src/components/Submitpage/index.js
@@ -1,28 +1,53 @@
+import {useNavigate, useLocation} from 'react-router-dom'
+import {useState, useEffect} from 'react'
 import Navbar from '../Navbar'
 import './index.css'
-import {useNavigate, useLocation} from 'react-router-dom'
 
 const SubmitAssessment = () => {
-    const navigate = useNavigate()
-    const location = useLocation()
-    const {score, time} = location.state
-    console.log(time)
-    const reattemptSection = () => {
-        navigate('/questions')
+  const [marks, setMarks] = useState(0)
+  const [timer, setTimer] = useState('00:00:00')
+  const navigate = useNavigate()
+  const location = useLocation()
+  useEffect(() => {
+    if (location.state === null) {
+      setMarks(0)
+      setTimer('00:00:00')
+    } else {
+      const {score, time} = location.state
+      setMarks(score)
+      setTimer(time)
     }
-    return(
-        <div>
-            <Navbar />
-            <div className='submit-assess-container'>
-                <img className='submit-image' src = 'https://res.cloudinary.com/dymvamc30/image/upload/v1713587725/Asset_2_1_vmgfoz.png' alt = "submit-img" />
-                <p className='congrats'>Congrats! You completed the Assessment</p>
-                <p>Time Taken: <span className='sub-time'>{time}</span></p>
-                <h3 className='your-score'>Your Score: <span className='score'>{score}</span></h3>
-                <button className='reattempt-btn' type = 'button' onClick={reattemptSection}>Reattempt</button>
-            </div>
-        </div>
-            
-    )
+  }, [])
+
+  const reattemptSection = () => {
+    navigate('/questions')
+  }
+  return (
+    <div>
+      <Navbar />
+      <div className="submit-assess-container">
+        <img
+          className="submit-image"
+          src="https://res.cloudinary.com/dymvamc30/image/upload/v1713587725/Asset_2_1_vmgfoz.png"
+          alt="submit-img"
+        />
+        <p className="congrats">Congrats! You completed the Assessment</p>
+        <p>
+          Time Taken: <span className="sub-time">{timer}</span>
+        </p>
+        <h3 className="your-score">
+          Your Score: <span className="score">{marks}</span>
+        </h3>
+        <button
+          className="reattempt-btn"
+          type="button"
+          onClick={reattemptSection}
+        >
+          Reattempt
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default SubmitAssessment
